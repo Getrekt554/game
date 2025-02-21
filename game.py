cd2935118722ef96fc16db9fa93b2fae852936e1
Cortland <gtrektlol@gmail.com>
2025-02-21 00:36:25 -0600

    test with git functionality

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((800, 800))
 run  = True
-
+print("hello world!")
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
