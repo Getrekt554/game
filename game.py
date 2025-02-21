8f8d84186bbd42465965bb957ed7affa213b2103
Lolbix69 <rbdproductionz@gmail.com>
2025-02-21 00:50:52 -0600

    added clock

--- game.py
+++ game.py
@@ -2,11 +2,14 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((800, 800))
+clock = pygame.time.Clock()
 run  = True
 
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    
+    clock.tick(60)
 
 pygame.quit()
